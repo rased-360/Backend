bb7531e9fc38b218c8d97902334589c834999075
hananazmy972 <hananazmy712@gmail.com>
2025-10-26 23:16:00 +0300

    Initial project setup with layered architecture and dependencies

---
+++ RaSed.Infrastructure/Data/Context/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RaSed.Infrastructure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaSed.Infrastructure.Data.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected AppDbContext() : base()
+        {
+            // This constructor is protected to prevent instantiation without options.
+            // It can be used for testing purposes or when creating a derived context.
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+    }
+}
---
+++ RaSed.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaSed.Infrastructure.Identity
+{
+    public class ApplicationUser: IdentityUser<int>
+    {
+
+    }
+}
