a0e4cb6ca46c232a1263e4465ae8c8b8533ad629
fatma-2117 <fatma.ahmed2202@gmail.com>
2025-10-28 19:54:24 +0300

    Implement essential Dto for log in one for request and the other for response

---
+++ RaSed.Application/DTOs/Authantication/LoginResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaSed.Application.DTOs.Authantication
+{
+    public class LoginResponseDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public bool MustChangePassword { get; set; }
+        public AdminResponseDto? Admin { get; set; }
+
+    }
+}
