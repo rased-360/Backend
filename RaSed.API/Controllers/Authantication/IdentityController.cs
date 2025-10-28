0c2637443060068105b39a2a07f8bd635ec00b53
fatma-2117 <fatma.ahmed2202@gmail.com>
2025-10-28 19:56:00 +0300

    implement the login and the logout endpoints

---
+++ RaSed.API/Controllers/Authantication/IdentityController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RaSed.Application.DTOs.Authantication;
+using RaSed.Application.Interfaces;
+
+namespace RaSed.API.Controllers.Authantication
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+        public IdentityController(IIdentityService _identityService)
+        {
+            this._identityService = _identityService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            try
+            {
+                var response = await _identityService.LoginAsync(dto);
+                return Ok(response);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            try
+            {
+
+                return Ok(new { message = "Logged out successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Error while logging out.", details = ex.Message });
+            }
+        }
+    }
+}
