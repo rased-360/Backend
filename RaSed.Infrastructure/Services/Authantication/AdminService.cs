327e2fba1ec7946333b2883ac8d2e3d47ab581eb
fatma-2117 <fatma.ahmed2202@gmail.com>
2025-10-28 19:12:14 +0300

    implement the Crud operation in Admin service to Edit ,Get,Delete Admin

--- RaSed.Infrastructure/Services/Authantication/AdminService.cs
+++ RaSed.Infrastructure/Services/Authantication/AdminService.cs
@@ -25,6 +25,7 @@ namespace RaSed.Infrastructure.Services.Authantication
             this._userManager = _userManager;
         }
 
+        //Create Admin
         public async Task<CreateAdminResponseDto> CreateAdminAsync(CreateAdminDto dto)
         {
             try
@@ -82,5 +83,169 @@ namespace RaSed.Infrastructure.Services.Authantication
                 throw new Exception("An unexpected error occurred while creating the admin. Please try again later.");
             }
         }
+
+        //Get Admin by Id
+        public async Task<AdminResponseDto?> GetAdminByIdAsync(int id)
+        {
+            try
+            {
+                var admin = await _unitOfWork._adminReposatory.GetByIdAsync(id);
+                var result = new AdminResponseDto
+                {
+                    Success = true,
+                    Id = admin.Id,
+                    Email = admin.Email,
+                    FullName = admin.FullName,
+                    PhoneNumber = admin.PhoneNumber,
+                    Gender = admin.Gender,
+                    NationalId = admin.NationalId,
+                    IsSuperAdmin = admin.IsSuperAdmin,
+                    IsActive = admin.IsActive,
+                    CreatedAt = admin.CreatedAt,
+                };
+                return admin != null ? result : null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong to gat this Admin data {id}", ex);
+            }
+        }
+
+        //Get Admin by Email
+        public async Task<AdminResponseDto?> GetAdminByEmailAsync(string email)
+        {
+            try
+            {
+                var admin = await _unitOfWork._adminReposatory.GetAdminByEmailAsync(email);
+                var result = new AdminResponseDto
+                {
+                    Success = true,
+                    Message = "Admin created successfully.",
+                    Id = admin.Id,
+                    Email = admin.Email,
+                    FullName = admin.FullName,
+                    PhoneNumber = admin.PhoneNumber,
+                    Gender = admin.Gender,
+                    NationalId = admin.NationalId,
+                    IsSuperAdmin = admin.IsSuperAdmin,
+                    IsActive = admin.IsActive,
+                    CreatedAt = admin.CreatedAt,
+                };
+                return admin != null ? result : null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong to gat this Admin data {email}", ex);
+            }
+        }
+
+        //Get all Admins
+        public async Task<IEnumerable<AdminResponseDto>> GetAllAdminsAsync()
+        {
+            try
+            {
+                var allAdmins = await _unitOfWork._adminReposatory.GetAllAsync();
+
+            var admins = allAdmins.Where(admin =>!admin.IsSuperAdmin);
+
+            return admins.Select(admins => new AdminResponseDto
+            {
+                Success = true,
+                Message = "Admin created successfully.",
+                Id = admins.Id,
+                Email = admins.Email,
+                FullName = admins.FullName,
+                PhoneNumber = admins.PhoneNumber,
+                Gender = admins.Gender,
+                NationalId = admins.NationalId,
+                IsSuperAdmin = admins.IsSuperAdmin,
+                IsActive = admins.IsActive,
+                CreatedAt = admins.CreatedAt,
+            });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Something went wrong to gat all Admins data", ex);
+            }
+
+        }
+
+        public async Task<AdminEditResponsDto> EditAdminAsync(int adminId, AdminEditDto editDto)
+        {
+            try
+            {
+                var existingAdmin = await _unitOfWork._adminReposatory.GetByIdAsync(adminId);
+
+                if (existingAdmin == null)
+                {
+                    throw new KeyNotFoundException("Admin not found.");
+                }
+
+                if (existingAdmin.IsSuperAdmin)
+                {
+                    throw new UnauthorizedAccessException("Cannot Edit a SuperAdmin.");
+                }
+
+                existingAdmin.FullName = editDto.FullName;
+                existingAdmin.Email = editDto.Email;
+                existingAdmin.Gender = editDto.Gender;
+                existingAdmin.NationalId = editDto.NationalId;
+                existingAdmin.PhoneNumber = editDto.PhoneNumber;
+                existingAdmin.DateOfBirth = editDto.DateOfBirth;
+                existingAdmin.HireType = editDto.HireType;
+
+                var updateResult = await _userManager.UpdateAsync(existingAdmin);
+
+                if (!updateResult.Succeeded)
+                {
+                    var errors = string.Join(", ", updateResult.Errors.Select(e => e.Description));
+                    throw new InvalidOperationException($"Fail in updating data {errors}");
+                }
+
+                await _unitOfWork.SaveChangesAsync();
+
+                var result = new AdminEditResponsDto
+                {
+                    Success = true,
+                    Message = "Admin created successfully.",
+                    Email = editDto.Email,
+                    FullName = editDto.FullName,
+                    PhoneNumber = editDto.PhoneNumber,
+                    Gender = editDto.Gender,
+                    NationalId = editDto.NationalId,
+
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fail in updating data", ex);
+            }
+        }
+        
+
+        //Delete Admin by Id
+        public async Task<bool> DeleteAdminByIdAsync(int id)
+        {
+            var adminToDelete = await _unitOfWork._adminReposatory.GetByIdAsync(id);
+
+            if (adminToDelete == null)
+            {
+                throw new KeyNotFoundException("Admin not found.");
+            }
+
+            if (adminToDelete.IsSuperAdmin)
+            {
+                throw new InvalidOperationException("Cannot delete a super admin.");
+
+            }
+
+            _unitOfWork._adminReposatory.Delete(adminToDelete);
+            await _unitOfWork.SaveChangesAsync();
+
+            return true;
+        }
+
+
     }
 }
