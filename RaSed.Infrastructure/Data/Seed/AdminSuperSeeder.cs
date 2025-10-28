8660802beddb945e19c585a5f4ca548d4d492623
fatma-2117 <fatma.ahmed2202@gmail.com>
2025-10-28 15:38:19 +0300

    Add the seed folder to create the Roles ["SuperAdmin", "Admin", "Employee"] in the applicationRule , creating the super admine data in the data base

---
+++ RaSed.Infrastructure/Data/Seed/AdminSuperSeeder.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RaSed.Domain.Entities;
+using RaSed.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaSed.Infrastructure.Data.Seed
+{
+    public class AdminSuperSeeder
+    {
+        public static async Task SeedSuperAdminAsync(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationRole> roleManager)
+        {
+            // بنتاكد إن الـ Roles موجودة
+            await RolesSeeder.SeedRolesAsync(roleManager);
+
+            var superAdminEmail = "superadmin@factory.com";
+
+            var existingAdmin = await userManager.FindByEmailAsync(superAdminEmail);
+            if (existingAdmin != null) return; // موجود فعلاً
+
+            var superAdmin = new Admin
+            {
+                Email = "superadmin@gmail.com",
+                UserName = "superadmin@gmail.com",
+                FullName = "Super Administrator",
+                PhoneNumber = "01000000000",
+                Gender = Gender.Male,
+                NationalId = "00000000000000",
+                DateOfBirth = DateTime.SpecifyKind(new DateTime(1990, 1, 1), DateTimeKind.Utc),
+                HireType = HireType.FullTime,
+                IsActive = true,
+                CreatedAt = DateTime.UtcNow,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsSuperAdmin = true
+            };
+
+            var result = await userManager.CreateAsync(superAdmin, "Super@1234");
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(superAdmin, "SuperAdmin");
+            }
+        }
+    }
+}
---
+++ RaSed.Infrastructure/Data/Seed/RolesSeeder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RaSed.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaSed.Infrastructure.Data.Seed
+{
+    public class RolesSeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<ApplicationRole> roleManager)
+        {
+
+            string[] roleNames = { "SuperAdmin", "Admin", "Employee" };
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new ApplicationRole { Name = roleName });
+                }
+            }
+        }
+    }
+}
